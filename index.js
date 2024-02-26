39ea4d510d2315f2253cad0150cb7c1ae11f0bd6
Abdi kumela <abdikumela@036gmail.com>
2024-02-26 06:22:35 -0800

    added module

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
-
-// Servers
-
-// function to repalce the html
-function replaceTemplate(temp, product) {
-  let outPut = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  outPut = outPut.replace(/{%IMAGE%}/g, product.image);
-  outPut = outPut.replace(/{%PRICE%}/g, product.price);
-  outPut = outPut.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  outPut = outPut.replace(/{%FROM%}/g, product.from);
-  outPut = outPut.replace(/{%QUANTITY%}/g, product.quantity);
-  outPut = outPut.replace(/{%DESCRIPTION%}/g, product.description);
-  outPut = outPut.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic)
-    outPut = outPut.replace(/{%NOT_ORGANIC%}/g, "not_organic");
-  return outPut;
-}
+const replaceTemplate = require("./modules/replaceTemplate");
+// Server
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/overview.html`,
