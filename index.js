5379d92cc71edb13dc26cb0da702960f87492510
Abdi kumela <abdikumela@036gmail.com>
2024-02-25 18:10:40 -0800

    implemented the API for Overview

--- index.js
+++ index.js
@@ -1,3 +1,68 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+
+// Servers
+function replaceTemplate(temp, product) {
+  let outPut = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  outPut = outPut.replace(/{%IMAGE%}/g, product.image);
+  outPut = outPut.replace(/{%PRICE%}/g, product.price);
+  outPut = outPut.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  outPut = outPut.replace(/{%FROM%}/g, product.from);
+  outPut = outPut.replace(/{%QUANTITY%}/g, product.quantity);
+  outPut = outPut.replace(/{%DESCRIPTION%}/g, product.description);
+  outPut = outPut.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    outPut = outPut.replace(/{%NOT_ORGANIC%}/g, "not_organic");
+  return outPut;
+}
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template_card.html`,
+  "utf-8"
+);
+const product = fs.readFileSync(`${__dirname}/templates/product.html`, "utf-8");
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  // overview page
+  if (pathName === "/" || pathName === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const cardHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardHtml);
+    res.end(output);
+  }
+  //   prooduct page
+  else if (pathName === "/product") {
+    res.end("This is a PRODUCT");
+  }
+  //   API
+  else if (pathName === "/api") {
+    res,
+      writeHead(200, {
+        "Content-type": "application/json",
+      });
+    res.end(data);
+  } else {
+    res.writeHead(400, {
+      "Content-type": "text/html",
+    });
+    res.end("<h1>Page not Found</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listning to requests on port number 8000");
+});
